59889b73d89bc008ea01199000743972fc3ecd20
Jose Camarena <jose.j.camarena@gmail.com>
2025-05-06 19:40:17 -0700

    initialize project

---
+++ src/test/java/org/memories/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.memories.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
